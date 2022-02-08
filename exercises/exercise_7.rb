46c59e9bea6229641ef1d1fe18e0979c4ac24a42
priyaprecious <priyanka.datti@gmail.com>
2022-02-08 00:33:20 +0000

    exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,19 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :hourly_rate, numericality: { only_integer: true, greater_than: 39, less_than: 201}
+    validates :store_id, presence: true
+  end
+  
+  class Store < ActiveRecord::Base
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_intiger: true, greater_than: 0 }
+  end
+  
+  puts "Give me a store name please!"
+  @new_store = gets.chomp
+  Store.create(name: @new_store)
+  puts @new_store.errors.full_messages
